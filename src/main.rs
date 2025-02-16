14c629a2846c740b27c2587b7bc7fa148d4004ff
Chris Patuzzo <chris@patuzzo.co.uk>
2025-02-16 13:25:51 +0000

    Store u32 instead of u64 in sums_by_class and centers_to_check

--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,33 @@ fn main() {
 
     let mut squares_by_class = [vec![], vec![], vec![]];
     let mut sums_by_class = [map.clone(), map.clone(), map];
-    let mut sums_to_check = VecDeque::new();
+    let mut centers_to_check = VecDeque::new();
 
     for number in 1_u32.. {
         let square = number as u64 * number as u64;
         if square % 24 != 1 { continue; }
 
-        while let Some(sum) = sums_to_check.front() {
-            if *sum >= square + 1 { break; }
-            let sum = sums_to_check.pop_front().unwrap();
+        while let Some(&center) = centers_to_check.front() {
+            let center_square = center as u64 * center as u64;
+            let center_sum = center_square + center_square;
 
-            let center_square = sum / 2;
+            if center_sum >= square + 1 { break; }
+            centers_to_check.pop_front();
+
             let center_class = (center_square % 72 / 24) as usize;
-
             let complement_class = (6 - center_class) % 3;
+
             let sums = &sums_by_class[complement_class];
+            let Some(squares) = sums.get(&hash(center_sum)) else { continue };
 
-            let Some(squares) = sums.get(&hash(sum)) else { continue };
-
-            //println!("{} = {} + ... {:?}", center_square * 3, center_square, squares);
+            // println!("{} = {}, {:?}", center_square * 3, center, squares);
         }
 
         let center_sum = square + square;
         let center_sum_class = (center_sum % 72 / 24) as usize;
 
         sums_by_class[center_sum_class].insert(hash(center_sum), vec![]);
-        sums_to_check.push_back(center_sum);
+        centers_to_check.push_back(number);
 
         let square_class = (square % 72 / 24) as usize;
         let square_vector = Simd::splat(square);
@@ -52,21 +53,17 @@ fn main() {
 
             for chunk in chunks {
                 let sum_vector = square_vector + Simd::from_slice(chunk);
-                let hash_keys = parallel_hash(sum_vector);
-
-                for i in 0..LANES {
-                    if let Some(vec) = sums.get_mut(&hash_keys[i]) {
-                        vec.push(square);
+                for hash in parallel_hash(sum_vector).as_array() {
+                    if let Some(vec) = sums.get_mut(hash) {
+                        vec.push(number);
                     }
                 }
             }
 
             for &square2 in remainder {
                 let sum = square + square2;
-                let hash_key = hash(sum);
-
-                if let Some(vec) = sums.get_mut(&hash_key) {
-                    vec.push(square);
+                if let Some(vec) = sums.get_mut(&hash(sum)) {
+                    vec.push(number);
                 }
             }
         }
@@ -83,13 +80,11 @@ const PRIME_VECTOR: Simd::<u64, LANES> = Simd::splat(PRIME);
 fn parallel_hash(vector: Simd::<u64, LANES>) -> Simd::<u64, LANES> {
     let round1 = vector ^ (vector >> 33);
     let round2 = round1 * PRIME_VECTOR;
-    let round3 = round2 ^ (round2 >> 33);
-    round3
+    round2 ^ (round2 >> 33)
 }
 
 fn hash(value: u64) -> u64 {
     let round1 = value ^ value.wrapping_shr(33);
     let round2 = round1.wrapping_mul(PRIME);
-    let round3 = round2 ^ round2.wrapping_shr(33);
-    round3
+    round2 ^ round2.wrapping_shr(33)
 }
