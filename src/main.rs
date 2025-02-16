22ae4445eb46b33e67523abe4b76ecae21ebe4cd
Chris Patuzzo <chris@patuzzo.co.uk>
2025-02-16 18:59:11 +0000

    Write checkpoints to disk

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 
 use nohash::NoHashHasher;
 use rayon::prelude::*;
+use bincode::{serialize, deserialize};
+use serde::{Serialize, Serializer, Deserialize, Deserializer};
 use std::collections::{HashMap, VecDeque};
 use std::hash::BuildHasherDefault;
 use std::simd::Simd;
 use std::sync::{Arc, Mutex};
+use std::fs::{write, copy};
 
-const LANES: usize = 64;
+const SIMD_LANES: usize = 64;
+const CHECKPOINTS: u64 = 10_000_000_000_000;
 
 fn main() {
     let hasher = BuildHasherDefault::<NoHashHasher<u64>>::default();
@@ -16,6 +20,7 @@ fn main() {
     let mut squares_by_class = [vec![], vec![], vec![]];
     let mut sums_by_class = [map.clone(), map.clone(), map];
     let mut centers_to_check = VecDeque::new();
+    let mut next_checkpoint = CHECKPOINTS;
 
     for number in 1_u32.. {
         let square = number as u64 * number as u64;
@@ -42,7 +47,7 @@ fn main() {
         let center_sum = square + square;
         let center_sum_class = (center_sum % 72 / 24) as usize;
 
-        sums_by_class[center_sum_class].insert(hash(center_sum), Arc::new(Mutex::new(vec![])));
+        sums_by_class[center_sum_class].insert(hash(center_sum), SharedVec::default());
         centers_to_check.push_back(number);
 
         let square_class = (square % 72 / 24) as usize;
@@ -52,14 +57,14 @@ fn main() {
             let residue_class = (6 - square_class + i) % 3;
             let squares = &squares_by_class[residue_class];
 
-            let chunks = squares.par_chunks_exact(LANES);
+            let chunks = squares.par_chunks_exact(SIMD_LANES);
             let remainder = chunks.remainder();
 
             chunks.for_each(|chunk| {
                 let sum_vector = square_vector + Simd::from_slice(chunk);
                 for hash in parallel_hash(sum_vector).as_array() {
                     if let Some(vec) = sums.get(hash) {
-                        vec.lock().unwrap().push(number);
+                        vec.0.lock().unwrap().push(number);
                     }
                 }
             });
@@ -67,21 +72,29 @@ fn main() {
             for &square2 in remainder {
                 let sum = square + square2;
                 if let Some(vec) = sums.get_mut(&hash(sum)) {
-                    vec.lock().unwrap().push(number);
+                    vec.0.lock().unwrap().push(number);
                 }
             }
         });
 
         squares_by_class[square_class].push(square);
 
-        if number > 600_000 { break; }
+        if square >= next_checkpoint {
+            let bytes = serialize(&(&squares_by_class, &sums_by_class, &centers_to_check, next_checkpoint, number)).unwrap();
+            let _result = copy("checkpoint.bin", "checkpoint.backup.bin");
+
+            write("checkpoint.bin", &bytes).unwrap();
+            println!("checkpointed at magic sum {}", square);
+
+            next_checkpoint += CHECKPOINTS;
+        }
     }
 }
 
 const PRIME: u64 = 11400714785074694791;
-const PRIME_VECTOR: Simd::<u64, LANES> = Simd::splat(PRIME);
+const PRIME_VECTOR: Simd::<u64, SIMD_LANES> = Simd::splat(PRIME);
 
-fn parallel_hash(vector: Simd::<u64, LANES>) -> Simd::<u64, LANES> {
+fn parallel_hash(vector: Simd::<u64, SIMD_LANES>) -> Simd::<u64, SIMD_LANES> {
     let round1 = vector ^ (vector >> 33);
     let round2 = round1 * PRIME_VECTOR;
     round2 ^ (round2 >> 33)
@@ -91,4 +104,19 @@ fn hash(value: u64) -> u64 {
     let round1 = value ^ value.wrapping_shr(33);
     let round2 = round1.wrapping_mul(PRIME);
     round2 ^ round2.wrapping_shr(33)
+}
+
+#[derive(Clone, Default)]
+struct SharedVec(Arc<Mutex<Vec<u32>>>);
+
+impl Serialize for SharedVec {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        self.0.lock().unwrap().serialize(serializer)
+    }
+}
+
+impl<'a> Deserialize<'a> for SharedVec {
+    fn deserialize<D: Deserializer<'a>>(deserializer: D) -> Result<Self, D::Error> {
+        Ok(Self(Arc::new(Mutex::new(Vec::deserialize(deserializer).unwrap()))))
+    }
 }
