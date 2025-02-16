7a88b97cce0054103315a362d1bf3d28968e5267
Chris Patuzzo <chris@patuzzo.co.uk>
2025-02-16 19:54:37 +0000

    Resume from checkpoints on disk

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,41 @@ use std::collections::{HashMap, VecDeque};
 use std::hash::BuildHasherDefault;
 use std::simd::Simd;
 use std::sync::{Arc, Mutex};
-use std::fs::{write, copy};
+use std::fs::{read, write, copy};
 
 const SIMD_LANES: usize = 64;
 const CHECKPOINTS: u64 = 10_000_000_000_000;
 
 fn main() {
-    let hasher = BuildHasherDefault::<NoHashHasher<u64>>::default();
-    let map = HashMap::with_capacity_and_hasher(1_000_000, hasher);
+    let (mut squares_by_class, mut sums_by_class, mut centers_to_check, mut next_checkpoint, next_number) = match read("checkpoint.bin") {
+        Ok(bytes) => {
+            let (squares, sums, centers, checkpoint, number): (_, _, _, u64, u32) = deserialize(&bytes).unwrap();
+            let square = number as u64 * number as u64;
+            println!("Checked all magic sums below {}. Resumed checkpoint.", square);
 
-    let mut squares_by_class = [vec![], vec![], vec![]];
-    let mut sums_by_class = [map.clone(), map.clone(), map];
-    let mut centers_to_check = VecDeque::new();
-    let mut next_checkpoint = CHECKPOINTS;
+            (squares, sums, centers, checkpoint + CHECKPOINTS, number + 1)
+        },
+        Err(_) => {
+            println!("No checkpoint file found. Starting the search from scratch.");
+            let hasher = BuildHasherDefault::<NoHashHasher<u64>>::default();
+            let map = HashMap::with_capacity_and_hasher(1_000_000, hasher);
+            let squares_by_class = [vec![], vec![], vec![]];
+            let sums_by_class = [map.clone(), map.clone(), map];
+            let centers_to_check = VecDeque::new();
+            let next_checkpoint = CHECKPOINTS;
+            let next_number = 1;
 
-    for number in 1_u32.. {
+            (squares_by_class, sums_by_class, centers_to_check, next_checkpoint, next_number)
+        }
+    };
+
+    for number in next_number.. {
         let square = number as u64 * number as u64;
         if square % 24 != 1 { continue; }
 
         while let Some(&center) = centers_to_check.front() {
             let center_square = center as u64 * center as u64;
             let center_sum = center_square + center_square;
-
-            // println!("{}: {} billion", number, center_square * 3 / 1_000_000_000);
 
             if center_sum > square { break; }
             centers_to_check.pop_front();
@@ -39,9 +51,10 @@ fn main() {
             let complement_class = (6 - center_class) % 3;
 
             let sums = &mut sums_by_class[complement_class];
-            let Some(numbers) = sums.remove(&hash(center_sum)) else { continue };
+            let Some(SharedVec(numbers)) = sums.remove(&hash(center_sum)) else { continue };
 
-            // println!("{} = {}, {:?}", center_square * 3, center, numbers);
+            let numbers = Mutex::into_inner(Arc::into_inner(numbers).unwrap()).unwrap();
+            // println!("{}, {:?}", center, numbers);
         }
 
         let center_sum = square + square;
@@ -80,11 +93,13 @@ fn main() {
         squares_by_class[square_class].push(square);
 
         if square >= next_checkpoint {
-            let bytes = serialize(&(&squares_by_class, &sums_by_class, &centers_to_check, next_checkpoint, number)).unwrap();
+            print!("Checked all magic sums below {}. ", square);
+
             let _result = copy("checkpoint.bin", "checkpoint.backup.bin");
+            let bytes = serialize(&(&squares_by_class, &sums_by_class, &centers_to_check, next_checkpoint, number)).unwrap();
 
             write("checkpoint.bin", &bytes).unwrap();
-            println!("checkpointed at magic sum {}", square);
+            println!("Wrote checkpoint.");
 
             next_checkpoint += CHECKPOINTS;
         }
