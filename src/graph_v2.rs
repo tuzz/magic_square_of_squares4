f8ec764231f6bc8e260f70c1594b2e62f59ad3ee
Chris Patuzzo <chris@patuzzo.co.uk>
2025-02-20 13:52:24 +0000

    Populate MagicSums

--- src/graph_v2.rs
+++ src/graph_v2.rs
@@ -1,15 +1,17 @@
 use std::collections::HashMap;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
+use crate::MagicSums;
 
 pub struct Graph {
-    ordered_squares: Vec<Candidate>,
-    map: HashMap<Candidate, (IsSquare, Occurences)>,
-    additions1: Vec<(Candidate, IsSquare)>,
-    square_additions2: Vec<Candidate>,
-    non_square_additions3: Vec<Candidate>,
+    magic_sums: MagicSums,
+    ordered_squares: Vec<(Candidate, ElementIndex)>,
+    additions1: Vec<(Candidate, ElementIndex, IsSquare)>,
+    square_additions2: Vec<(Candidate, ElementIndex)>,
+    non_square_additions3: Vec<(Candidate, ElementIndex)>,
 }
 
 type Candidate = u64;
+type ElementIndex = usize;
 type IsSquare = bool;
 type Occurences = usize;
 
@@ -17,7 +19,7 @@ impl Graph {
     pub fn new() -> Self {
         Self {
             ordered_squares: vec![],
-            map: HashMap::new(),
+            magic_sums: MagicSums::default(),
             additions1: vec![],
             square_additions2: vec![],
             non_square_additions3: vec![],
@@ -25,8 +27,8 @@ impl Graph {
     }
 
     pub fn generate(&mut self, center_square: u64, bigger_numbers: Vec<u32>) {
+        self.magic_sums.clear();
         self.ordered_squares.clear();
-        self.map.clear();
         self.additions1.clear();
         self.square_additions2.clear();
         self.non_square_additions3.clear();
@@ -36,90 +38,86 @@ impl Graph {
 
         let bigger_squares = bigger_numbers.iter().map(|&n| n as u64 * n as u64);
         let smaller_squares = bigger_squares.clone().map(|s| center_sum - s);
+        let ordered_squares = smaller_squares.rev().chain(bigger_squares);
 
-        self.ordered_squares.extend(smaller_squares.rev().chain(bigger_squares));
-        self.map.extend(self.ordered_squares.iter().map(|&s| (s, (true, 1))));
+        for square in ordered_squares {
+            let (i, _) = self.magic_sums.get_or_insert_element(square, || true, |_| true).unwrap();
+            self.ordered_squares.push((square, i));
+        }
 
-        'outer: for (i, &square1) in self.ordered_squares.iter().enumerate() {
+        'outer: for &(square1, i) in &self.ordered_squares {
             let remainder = magic_sum - square1;
 
-            for (j, &square2) in self.ordered_squares[i + 1..].iter().enumerate() {
+            for &(square2, j) in &self.ordered_squares[i + 1..] {
                 let candidate = remainder.saturating_sub(square2);
                 if candidate == 0 { if j == 0 { break 'outer; } else { break; } }
 
-                self.map.entry(candidate)
-                    .and_modify(|(_is_square, occurences)| *occurences += 1)
-                    .or_insert_with(|| {
-                        let is_square = is_perfect_square(candidate);
-                        self.additions1.push((candidate, is_square));
-                        (is_square, 1)
-                    });
+                let condition = |_| true;
+                let option = self.magic_sums.get_or_insert_element(candidate, || is_square(candidate), condition);
+                let (k, is_square) = option.unwrap();
+
+                self.magic_sums.add_partnership(i, j, k);
+                self.additions1.push((candidate, k, is_square));
             }
         }
 
-        self.additions1.sort();
+        self.additions1.sort_by_key(|&(candidate, _, _)| candidate);
 
-        'outer: for (i, &square) in self.ordered_squares.iter().enumerate() {
+        'outer: for &(square, i) in &self.ordered_squares {
             let remainder = magic_sum - square;
 
-            for (j, &(addition, addition_is_square)) in self.additions1.iter().enumerate() {
+            for &(addition, j, addition_is_square) in &self.additions1 {
                 let candidate = remainder.saturating_sub(addition);
                 if candidate == 0 { if j == 0 { break 'outer; } else { break; } }
 
-                match self.map.entry(candidate) {
-                    Occupied(mut entry) => entry.get_mut().1 += 1,
-                    Vacant(entry) => {
-                        let candidate_is_square = is_perfect_square(candidate);
-                        if candidate_is_square || addition_is_square {
-                            entry.insert((candidate_is_square, 1));
-                            if candidate_is_square { self.square_additions2.push(candidate); }
-                        }
-                    },
+                let condition = |candidate_is_square| { candidate_is_square || addition_is_square };
+                let option = self.magic_sums.get_or_insert_element(candidate, || is_square(candidate), condition);
+
+                if let Some((k, _)) = option {
+                    self.magic_sums.add_partnership(i, j, k);
+                    self.square_additions2.push((candidate, k));
                 }
             }
         }
 
         self.square_additions2.sort();
 
-        'outer: for (i, &square) in self.ordered_squares.iter().enumerate() {
+        'outer: for &(square, i) in &self.ordered_squares {
             let remainder = magic_sum - square;
 
-            for (j, &addition) in self.square_additions2.iter().enumerate() {
+            for &(addition, j) in &self.square_additions2 {
                 let candidate = remainder.saturating_sub(addition);
                 if candidate == 0 { if j == 0 { break 'outer; } else { break; } }
 
-                self.map.entry(candidate)
-                    .and_modify(|(_is_square, occurences)| *occurences += 1)
-                    .or_insert_with(|| {
-                        let is_square = is_perfect_square(candidate);
-                        if !is_square { self.non_square_additions3.push(candidate); }
-                        (is_square, 1)
-                    });
+                let condition = |candidate_is_square: bool| { !candidate_is_square };
+                let option = self.magic_sums.get_or_insert_element(candidate, || is_square(candidate), condition);
+
+                if let Some((k, _)) = option {
+                    self.magic_sums.add_partnership(i, j, k);
+                    self.non_square_additions3.push((candidate, k));
+                }
             }
         }
 
         self.non_square_additions3.sort();
 
-        'outer: for (i, &square) in self.ordered_squares.iter().enumerate() {
+        'outer: for &(square, i) in &self.ordered_squares {
             let remainder = magic_sum - square;
 
-            for (j, &addition) in self.non_square_additions3.iter().enumerate() {
+            for &(addition, j) in &self.non_square_additions3 {
                 let candidate = remainder.saturating_sub(addition);
                 if candidate == 0 { if j == 0 { break 'outer; } else { break; } }
 
-                match self.map.entry(candidate) {
-                    Occupied(mut entry) => entry.get_mut().1 += 1,
-                    Vacant(entry) => {
-                        let is_square = is_perfect_square(candidate);
-                        if is_square { entry.insert((is_square, 1)); }
-                    },
-                }
+                let condition = |candidate_is_square| candidate_is_square;
+                self.magic_sums.get_or_insert_element(candidate, || is_square(candidate), condition);
             }
         }
+
+        self.magic_sums.remove_if_less_than_two_partners();
     }
 }
 
-fn is_perfect_square(number: u64) -> bool {
+fn is_square(number: u64) -> bool {
     let root = number.isqrt();
     root * root == number
 }
--- src/magic_sums.rs
+++ src/magic_sums.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::mem::swap;
 
 #[derive(Default)]
@@ -15,11 +16,32 @@ struct Element {
 }
 
 impl MagicSums {
-    pub fn get_or_insert_element(&mut self, number: u64, is_square: bool) -> usize {
-        *self.lookup.entry(number).or_insert_with(|| {
-            self.elements.push(Element { number, is_square, partners: vec![] });
-            self.elements.len() - 1
-        })
+    pub fn get_or_insert_element<F, G>(&mut self, number: u64, is_square_fn: F, condition_fn: G) -> Option<(usize, bool)>
+        where F: Fn() -> bool,
+              G: Fn(bool) -> bool
+    {
+        match self.lookup.entry(number) {
+            Occupied(entry) => {
+                let index = *entry.get();
+                let is_square = self.elements[index].is_square;
+
+                Some((index, is_square))
+            },
+            Vacant(entry) => {
+                let is_square = is_square_fn();
+
+                if condition_fn(is_square) {
+                    let index = self.elements.len();
+
+                    self.elements.push(Element { number, is_square, partners: vec![] });
+                    entry.insert(index);
+
+                    Some((index, is_square))
+                } else {
+                    None
+                }
+            },
+        }
     }
 
     pub fn add_partnership(&mut self, index1: usize, index2: usize, index3: usize) {
@@ -35,7 +57,7 @@ impl MagicSums {
             for i in 0..self.elements.len() {
                 let element = &mut self.elements[i];
 
-                if element.partners.len() < 2 {
+                if element.partners.len() == 1 {
                     self.lookup.remove(&element.number);
                     swap(&mut element.partners, &mut self.tmp);
 
@@ -49,5 +71,10 @@ impl MagicSums {
 
             if !removed { break; }
         }
+    }
+
+    pub fn clear(&mut self) {
+        self.elements.clear();
+        self.lookup.clear();
     }
 }
